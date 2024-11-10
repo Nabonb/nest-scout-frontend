f7877e16ea7903f0129f4326d601ff7d35ae9468
Nabonb <nabobaruanb@gmail.com>
2024-11-10 21:55:55 +0600

    navbar full done with responsiveness

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,5 +1,8 @@
+import { useState } from "react";
 import "./navbar.scss";
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
+  console.log(open);
   return (
     <nav>
       <div className="left">
@@ -17,6 +20,21 @@ const Navbar = () => {
         <a className="register" href="/">
           Sign up
         </a>
+        <div className="menuIcon">
+          <img
+            src="/menu.png"
+            alt=""
+            onClick={() => setOpen((prev) => !prev)}
+          />
+        </div>
+        <div className={open ? "menu active" : "menu"}>
+          <a href="">Home</a>
+          <a href="">About</a>
+          <a href="">Contact</a>
+          <a href="">Agents</a>
+          <a href="">Sign in</a>
+          <a href="">Sign up</a>
+        </div>
       </div>
     </nav>
   );
