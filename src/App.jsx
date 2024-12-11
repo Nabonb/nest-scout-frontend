162a563a471d098464c9a9ef07db4887056705bc
Nabonb <nabobaruanb@gmail.com>
2024-12-11 12:35:17 +0600

    updated user information succesfully

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import SinglePage from "./routes/singlePage/SinglePage";
 import ProfilePage from "./routes/profilePage/ProfilePage";
 import Login from "./routes/login/Login";
 import Register from "./routes/register/Register";
+import ProfileUpdatePage from "./routes/profileUpdatePage/profileUpdatePage";
 function App() {
   const router = createBrowserRouter([
     {
@@ -41,6 +42,10 @@ function App() {
         {
           path: "/profile",
           element: <ProfilePage />,
+        },
+        {
+          path: "/profile/update",
+          element: <ProfileUpdatePage />,
         },
       ],
     },
